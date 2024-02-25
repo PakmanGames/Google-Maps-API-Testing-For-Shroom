de9d3efc4ea074029f546abc5d6bd7789ea81c1c
Andy Pak <im.andy.pak@gmail.com>
2024-02-24 20:43:58 -0500

    Fixed map display center to be based on users wifi location

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+var map;
+
 function initMap() {
-    // Map center & zoom
-    var options = {
-        center: {lat: 43.66090177575299, lng: -79.39639225889357},
-        zoom: 15
+    // Grab the user's location
+    x = navigator.geolocation;
+    x.getCurrentPosition(success, failure, { enableHighAccuracy: true });
+
+    // If the user's location is successfully grabbed, the success function is called
+    function success(position) {
+        var myLat = position.coords.latitude;
+        var myLong = position.coords.longitude;
+
+        // Map center & zoom
+        var options = {
+            center: {lat: myLat, lng: myLong},
+            zoom: 15,
+            mapId: "7219d2cdaaaa0b81"
+        }
+        // Create new map
+        map = new google.maps.Map(document.getElementById(`map`),options)
+
+    var coords = {lat: myLat, lng: myLong}; // Initialize coords object with lat and lng properties
+    addMarker({location: {lat: myLat, lng: myLong}, imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
+    content: `<h3>You are here!</h3>`});
     }
 
-    // Create new map
-    map = new google.maps.Map(document.getElementById(`map`),options)
-
-
-
+    function failure() {}
 
 
 
@@ -21,8 +36,7 @@ function initMap() {
 
 
 
-
-    
+    /*
     // Marker
     const marker = new google.maps.Marker({
         position:{lat: 43.66090177575299, lng: -79.39639225889357},
@@ -37,36 +51,39 @@ function initMap() {
         detailWindow.open(map, marker);
     })
     //
+    */
+
+
 
     // Add multiple markers with custon icons / content
-    function addMarker(property) {
-       const marker = new google.maps.Marker({
-        position: property.location,
-        map: map,
-        icon: property.imageIcon
-       }); 
+function addMarker(property) {
+    const marker = new google.maps.Marker({
+     position: property.location,
+     map: map,
+     icon: property.imageIcon
+    }); 
 
-       if (property.imageIcon) {
-        marker.setIcon(property.imageIcon);
-       }
-
-       const detailWindow = new google.maps.InfoWindow({
-        content: property.content
-       });
-
-       marker.addListener("click", () => {
-        detailWindow.open(map, marker);
-       });
+    if (property.imageIcon) {
+     marker.setIcon(property.imageIcon);
     }
 
-    // Array to store the markers
-    MarkerArray = [{location: {lat: 43.665213685270665, lng: -79.3955855605278}, imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
-    content: `<h3>Library</h3>`},
-    
-    {location: {lat: 43.66634369277465, lng: -79.39122269931296}}]
+    const detailWindow = new google.maps.InfoWindow({
+     content: property.content
+    });
 
-    // Loop through markers
-    for (let i = 0; i < MarkerArray.length; i++) {
-        addMarker(MarkerArray[i]);
-    }
+    marker.addListener("click", () => {
+     detailWindow.open(map, marker);
+    });
+ }
+
+ // Array to store the markers
+ MarkerArray = [{location: {lat: 43.665213685270665, lng: -74.3955855605278}, imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
+ content: `<h3>Library</h3>`},
+ 
+ {location: {lat: 43.66634369277465, lng: -79.39122269931296}}]
+
+ // Loop through markers
+ for (let i = 0; i < MarkerArray.length; i++) {
+     addMarker(MarkerArray[i]);
+ }
 }

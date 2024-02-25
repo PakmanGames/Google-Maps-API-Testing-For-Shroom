c295021537d84bab9c6cb57f935dd91e8956504e
Andy Pak <im.andy.pak@gmail.com>
2024-02-24 21:31:28 -0500

    Add failure function

--- app.js
+++ app.js
@@ -1,89 +1,82 @@
-var map;
+var map; // Initialize map variable
 
+// Initialize the map
 function initMap() {
     // Grab the user's location
     x = navigator.geolocation;
+    // If the user's location is successfully grabbed, the success function is called
     x.getCurrentPosition(success, failure, { enableHighAccuracy: true });
 
-    // If the user's location is successfully grabbed, the success function is called
+    // Sets the center of the map to be where the user is located
     function success(position) {
+        // Grab the user's latitude and longitude based on Wifi Access Point
         var myLat = position.coords.latitude;
         var myLong = position.coords.longitude;
 
         // Map center & zoom
         var options = {
-            center: {lat: myLat, lng: myLong},
-            zoom: 15,
-            mapId: "7219d2cdaaaa0b81"
+            center: {lat: myLat, lng: myLong}, // Set map center to user's location
+            zoom: 15, // Default zoom
+            mapId: "7219d2cdaaaa0b81", // Custom map style ID
+            minZoom: 13, // Minimum zoom
+            maxZoomL: 15.1 // Maximum zoom
         }
+
+        // Create new map
+        map = new google.maps.Map(document.getElementById(`map`),options)
+
+        var coords = {lat: myLat, lng: myLong}; // Initialize coords object with lat and lng properties
+        // Add marker for user's location with custom marker icon and detail window content
+        addMarker({location: {lat: myLat, lng: myLong}, 
+        imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
+        content: `<h3>You are here!</h3>`});
+    }
+
+    // If the user's location is not successfully grabbed, the failure function is called
+    function failure() {
+        var myLat = 43.66088234260811;
+        var myLong = -79.39609722039485;
+
+        // Map center & zoom
+        var options = {
+            center: {lat: myLat, lng: myLong}, // Set map center to user's location
+            zoom: 15, // Default zoom
+            mapId: "7219d2cdaaaa0b81", // Custom map style ID
+            minZoom: 13, // Minimum zoom
+            maxZoomL: 15.1 // Maximum zoom
+        }
+
         // Create new map
         map = new google.maps.Map(document.getElementById(`map`),options)
 
     var coords = {lat: myLat, lng: myLong}; // Initialize coords object with lat and lng properties
-    addMarker({location: {lat: myLat, lng: myLong}, imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
-    content: `<h3>You are here!</h3>`});
+    // Add marker for default location and create detail window content
+    addMarker({location: {lat: myLat, lng: myLong}, 
+    content: `<h3>Default Location</h3>`});
     }
-
-    function failure() {}
-
-
-
-    // Listen for click on map location
-    google.maps.event.addListener(map, "click", (event) => {
-        addMarker({location:event.latLng});
-    });
-
-
-
-
-    /*
-    // Marker
-    const marker = new google.maps.Marker({
-        position:{lat: 43.66090177575299, lng: -79.39639225889357},
-        map:map,
-        icon:"https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true"
-    });
-
-    const detailWindow = new google.maps.InfoWindow({
-        content: `<h3>You are here!</h3>`
-    });
-    marker.addListener("click", () =>{
-        detailWindow.open(map, marker);
-    })
-    //
-    */
-
-
 
     // Add multiple markers with custon icons / content
-function addMarker(property) {
-    const marker = new google.maps.Marker({
-     position: property.location,
-     map: map,
-     icon: property.imageIcon
-    }); 
+    function addMarker(property) {
+        // Create new marker with custom icon and content
+        const marker = new google.maps.Marker({
+        position: property.location,
+        map: map,
+        icon: property.imageIcon
+        }); 
 
-    if (property.imageIcon) {
-     marker.setIcon(property.imageIcon);
+        // If the marker has an icon, set the icon to the custom icon
+        if (property.imageIcon) {
+            marker.setIcon(property.imageIcon);
+        }
+
+        // If the marker has content, create a new detail window with the content
+        const detailWindow = new google.maps.InfoWindow({
+            content: property.content
+        });
+
+        // If the marker is clicked, open the detail window
+        marker.addListener("click", () => {
+            detailWindow.open(map, marker);
+        });
     }
-
-    const detailWindow = new google.maps.InfoWindow({
-     content: property.content
-    });
-
-    marker.addListener("click", () => {
-     detailWindow.open(map, marker);
-    });
- }
-
- // Array to store the markers
- MarkerArray = [{location: {lat: 43.665213685270665, lng: -74.3955855605278}, imageIcon: "https://github.com/PakmanGames/Stroom-Google-Maps-API/blob/main/YouAreHere50x50noBg.png?raw=true", 
- content: `<h3>Library</h3>`},
- 
- {location: {lat: 43.66634369277465, lng: -79.39122269931296}}]
-
- // Loop through markers
- for (let i = 0; i < MarkerArray.length; i++) {
-     addMarker(MarkerArray[i]);
- }
 }
